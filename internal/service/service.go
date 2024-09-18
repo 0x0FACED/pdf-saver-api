0e31b70ae086904d9ea8870a20dbaf71b2a6c5ca
0x0FACED <https.genius@gmail.com>
2024-09-18 13:59:21 +0300

    feat: added rod browser and mem to pdf service struct

--- internal/service/service.go
+++ internal/service/service.go
@@ -4,6 +4,8 @@ import (
 	"github.com/0x0FACED/pdf-proto/pdf_service/gen"
 	"github.com/0x0FACED/pdf-saver-api/config"
 	"github.com/0x0FACED/pdf-saver-api/internal/logger"
+	"github.com/0x0FACED/pdf-saver-api/internal/mem"
+	"github.com/go-rod/rod"
 )
 
 type PDFService struct {
@@ -11,11 +13,19 @@ type PDFService struct {
 
 	logger *logger.ZapLogger
 	cfg    config.PRFServiceConfig
+	// Здесь надо добавить еще headless браузер rod (go-rod)
+	// Еще реализовать механизм очереди через каналы
+	// добавить redis для хранения временных файлов
+
+	rod *rod.Browser
+	mem *mem.MemoryCacher
 }
 
-func New(logger *logger.ZapLogger, cfg config.PRFServiceConfig) *PDFService {
+func New(logger *logger.ZapLogger, cfg config.PRFServiceConfig, mem *mem.MemoryCacher) *PDFService {
 	return &PDFService{
 		logger: logger,
 		cfg:    cfg,
+		rod:    rod.New(),
+		mem:    mem,
 	}
 }
